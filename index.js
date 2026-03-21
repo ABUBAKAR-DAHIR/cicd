065ff550e64affce7626ee06ac8c2d96dacb0ce9
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-03-21 23:30:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const PORT = 9000;
+// const path = require("path")
+// const multer = require("multer")
+// const cloudinary = require("cloudinary").v2;
+const server = express();
+// require("dotenv").config()
+// server.use(express.static(path.resolve("public")))
+
+// const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
+
+// if (!CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET, !CLOUDINARY_CLOUD_NAME) {
+//     console.log("env not loaded")
+// }
+// else {
+//     console.log("env loaded")
+// }
+
+
+// const upload = multer({
+//     storage: multer.memoryStorage(),
+//     limits: { fileSize: 2 * 1024 * 1024 }
+// })
+
+// const UploadToCloudinary = async(buffer) =>{
+//     return new Promise((resolve , reject) => {
+//        const stream = cloudinary.uploader.upload_stream(
+//       { resource_type: "image" ,
+//         folder : "krishna-uploads"
+//       }, 
+//       (error, result) => {
+//         if (error) {
+//           reject(error);
+//         } else {
+//           resolve(result); 
+//         }
+//       }
+//     );
+//         stream.end(buffer)
+//     })
+// }
+
+// cloudinary.config({
+//     cloud_name: CLOUDINARY_CLOUD_NAME,
+//     api_key: CLOUDINARY_API_KEY,
+//     api_secret: CLOUDINARY_API_SECRET
+// }
+// )
+
+server.get("/upload-image" , (req , res) =>{
+    // try {
+    //     if(!req.file){
+    //         return res.json({message : "No files"})
+    //     }
+    //   const response = await UploadToCloudinary(req.file.buffer)
+    //   console.log(response)        
+    // } catch (error) {
+    //     return res.status(500).json({message : error})
+    // }
+
+    res.send("working")
+})
+
+server.listen(PORT, () => {
+    console.log("Server started on 9000")
+})
