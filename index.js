c586db31dfbc9be1e66663c40756ce0c26b91539
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-03-23 00:14:55 +0530

    adding time

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ server.get("/upload-image" , (req , res) =>{
     //     return res.status(500).json({message : error})
     // }
 
-    const date = new Date()
+    const date = new Date().toLocaleTimeString("en-IN", {
+        timeZone: "Asia/Kolkata",
+        hour12: false
+    })
 
     const day = String(date.getDate()).padStart(2, '0')
     const month = String(date.getMonth() + 1).padStart(2, '0')
@@ -67,8 +70,14 @@ server.get("/upload-image" , (req , res) =>{
     const current = `${day} ${month} ${year}`
 
     console.log(current) // 22 03 2026
+    
+    const hours = String(date.getHours()).padStart(2, "0")
+    const mins = String(date.getMinutes()).padStart(2, "0")
+    const secs = String(date.getSeconds()).padStart(2, "0")
 
-    res.json({message: "working done by abubakar", date: current})
+    const time = `${hours}:${mins}:${secs}`
+
+    res.json({message: "working done by abubakar", date: current, time: time})
 })
 
 server.listen(PORT, "0.0.0.0", () => {
